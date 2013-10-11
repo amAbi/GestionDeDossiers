0c3374e35883f610bd4ce4093790ade92b9f3b30
amAbi <abida.amine0@gmail.com>
2013-10-11 18:25:27 +0100

    Base de données modifiée

--- DB/begl.sql
+++ DB/begl.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 11 Octobre 2013 à 15:33
+-- Généré le: Ven 11 Octobre 2013 à 17:23
 -- Version du serveur: 5.5.24-log
 -- Version de PHP: 5.4.3
 
@@ -27,16 +27,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `acteur_chaine_instruction` (
-  `CIN` int(11) NOT NULL DEFAULT '0',
+  `CIN` int(11) NOT NULL,
   `NOM` varchar(100) DEFAULT NULL,
-  `PRENOM` varchar(100) DEFAULT NULL,
   `DEPARTEMENT` varchar(100) DEFAULT NULL,
-  `LOGIN` varchar(100) DEFAULT NULL,
-  `PASSWD` varchar(100) DEFAULT NULL,
-  `DECISION` text NOT NULL,
-  `AVIS` text NOT NULL,
-  PRIMARY KEY (`CIN`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `LOGIN` varchar(50) DEFAULT NULL,
+  `MOT_PASSE` varchar(50) DEFAULT NULL,
+  `DESICION` varchar(100) DEFAULT NULL,
+  `AVIS` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`CIN`),
+  UNIQUE KEY `ACTEUR_CHAINE_INSTRUCTION_PK` (`CIN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -45,12 +45,15 @@ CREATE TABLE IF NOT EXISTS `acteur_chaine_instruction` (
 --
 
 CREATE TABLE IF NOT EXISTS `article` (
-  `IDA` int(11) NOT NULL AUTO_INCREMENT,
+  `IDA` int(11) NOT NULL,
   `DATE` date DEFAULT NULL,
-  `CHEMIN` varchar(100) DEFAULT NULL,
+  `CHEMIN` varchar(1000) DEFAULT NULL,
   `NOM` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`IDA`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `IDF` int(11) NOT NULL,
+  PRIMARY KEY (`IDA`),
+  UNIQUE KEY `ARTICLE_PK` (`IDA`),
+  KEY `SE_COMPOSE_FK` (`IDF`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -59,27 +62,14 @@ CREATE TABLE IF NOT EXISTS `article` (
 --
 
 CREATE TABLE IF NOT EXISTS `enseignant_chercheur` (
-  `CIN` int(11) NOT NULL DEFAULT '0',
+  `CIN_E` int(11) NOT NULL,
   `NOM` varchar(100) DEFAULT NULL,
-  `PRENOM` varchar(100) DEFAULT NULL,
   `DEPARTEMENT` varchar(100) DEFAULT NULL,
-  `LOGIN` varchar(100) DEFAULT NULL,
-  `PASSWD` varchar(100) DEFAULT NULL,
-  `ETABLISSEMENT` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`CIN`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `formulaire`
---
-
-CREATE TABLE IF NOT EXISTS `formulaire` (
-  `IDF` int(11) NOT NULL AUTO_INCREMENT,
-  `DATE_F` date DEFAULT NULL,
-  PRIMARY KEY (`IDF`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `LOGIN` varchar(50) DEFAULT NULL,
+  `MOT_PASSE` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`CIN_E`),
+  UNIQUE KEY `ENSEIGNANT_CHERCHEUR_PK` (`CIN_E`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -88,39 +78,34 @@ CREATE TABLE IF NOT EXISTS `formulaire` (
 --
 
 CREATE TABLE IF NOT EXISTS `projet` (
-  `IDP` int(11) NOT NULL AUTO_INCREMENT,
-  `ETAT` varchar(100) NOT NULL,
-  `DATE_DEBUT` date NOT NULL,
-  PRIMARY KEY (`IDP`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `IDF` int(11) NOT NULL,
+  `CIN` int(11) DEFAULT NULL,
+  `CIN_E` int(11) NOT NULL,
+  `DATE` date DEFAULT NULL,
+  `ETAT` varchar(100) DEFAULT NULL,
+  `DATEDEB` date DEFAULT NULL,
+  PRIMARY KEY (`IDF`),
+  UNIQUE KEY `FORMULAIRE_PK` (`IDF`),
+  KEY `GERE_FK` (`CIN`),
+  KEY `REMPLIE_FK` (`CIN_E`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contraintes pour les tables exportées
 --
 
 --
--- Contraintes pour la table `acteur_chaine_instruction`
+-- Contraintes pour la table `article`
 --
-ALTER TABLE `acteur_chaine_instruction`
-  ADD CONSTRAINT `acteur_chaine_instruction_ibfk_1` FOREIGN KEY (`CIN`) REFERENCES `formulaire` (`IDF`);
-
---
--- Contraintes pour la table `enseignant_chercheur`
---
-ALTER TABLE `enseignant_chercheur`
-  ADD CONSTRAINT `enseignant_chercheur_ibfk_1` FOREIGN KEY (`CIN`) REFERENCES `formulaire` (`IDF`);
-
---
--- Contraintes pour la table `formulaire`
---
-ALTER TABLE `formulaire`
-  ADD CONSTRAINT `formulaire_ibfk_1` FOREIGN KEY (`IDF`) REFERENCES `projet` (`IDP`);
+ALTER TABLE `article`
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`IDF`) REFERENCES `projet` (`IDF`);
 
 --
 -- Contraintes pour la table `projet`
 --
 ALTER TABLE `projet`
-  ADD CONSTRAINT `projet_ibfk_2` FOREIGN KEY (`IDP`) REFERENCES `article` (`IDA`) ON DELETE CASCADE;
+  ADD CONSTRAINT `projet_ibfk_1` FOREIGN KEY (`CIN`) REFERENCES `acteur_chaine_instruction` (`CIN`),
+  ADD CONSTRAINT `projet_ibfk_2` FOREIGN KEY (`CIN_E`) REFERENCES `enseignant_chercheur` (`CIN_E`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
